bde0834df4a2a5b3fd1df679523ed3b2c01164db
Gregory Maddra <bloxgate@gmail.com>
2022-06-02 22:06:46 -0500

    Changes for 1.18.2 tag changes

---
+++ src/main/java/com/bloxgaming/tagfoods/mixins/MixinHolder.java
@@ -0,0 +1,27 @@
+package com.bloxgaming.tagfoods.mixins;
+
+import com.bloxgaming.tagfoods.IHolderWithModifiableTag;
+import com.google.common.collect.Sets;
+import net.minecraft.core.Holder;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Mixin(Holder.Reference.class)
+public class MixinHolder<T extends Item> implements IHolderWithModifiableTag<T> {
+
+    @Shadow
+    private Set<TagKey<T>> tags;
+
+    public void addTag(@NotNull TagKey<T> tag) {
+        //Tags will be immutable at runtime
+        HashSet<TagKey<T>> newTags = Sets.newHashSet(tags);
+        newTags.add(tag);
+        tags = newTags; //So we replace it instead
+    }
+}
